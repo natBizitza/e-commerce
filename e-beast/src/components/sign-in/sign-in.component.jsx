9c6b2ef114cd1b00311b3e3b0f2484fc8fb0a0c8
Natalia Romankevich <romankevich.law@gmail.com>
2020-10-30 19:40:17 +0100

    finishing sign in handleSubmit method

--- e-beast/src/components/sign-in/sign-in.component.jsx
+++ e-beast/src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from './../form-input/form-input.component';
 import CustomButton from './../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props){
@@ -14,10 +14,16 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = e => {
+    handleSubmit = async e => {
         e.preventDefault();
+        const {email, password} = this.state;
 
-        this.setState = ({email: '', password: ''})
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState = ({email: '', password: ''})
+        }catch(err){
+            console.log(err);
+        }
     }
 
     handleChange = e => {
