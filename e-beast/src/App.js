e7a9a91dbff59f2a974a65ba39486f713ceb88c1
Natalia Romankevich <romankevich.law@gmail.com>
2020-11-09 17:51:06 +0100

    added redux, and implemented userReducer and userActions to header and app components

--- e-beast/src/App.js
+++ e-beast/src/App.js
@@ -6,24 +6,21 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions'
+
 
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
@@ -31,7 +28,7 @@ class App extends React.Component {
           })
         })
       }else {
-        this.setState({currentUser: userAuth})
+        setCurrentUser({currentUser: userAuth})
       }
       
 
@@ -47,7 +44,7 @@ class App extends React.Component {
       <div>
         {/* placing outside of the Switch, cause it will be always rendered besides what component Switch decides 
         to render */}
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
@@ -58,4 +55,7 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+export default connect(null,mapDispatchToProps)(App);
--- e-beast/src/components/sign-in/sign-in.component.jsx
+++ e-beast/src/components/sign-in/sign-in.component.jsx
@@ -44,7 +44,7 @@ class SignIn extends React.Component {
                     handleChange={this.handleChange} label='`password'/>
                     <div className='buttons'>
                         <CustomButton type='submit'> Sign In </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>{''} Sign In with Google {''}</CustomButton>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>{''} Sign In with Google {''}</CustomButton>
                     </div>
                 </form>
             </div>
