f83414c59dace6b89fd5879d94fd75d27fd26238
Natalia Romankevich <romankevich.law@gmail.com>
2020-12-18 23:06:04 +0100

    react-persist for managing localStorage

--- e-beast/src/index.js
+++ e-beast/src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/es/integration/react';
+
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
--- e-beast/src/redux/root-reducer.js
+++ e-beast/src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+//localStorage access
+import  storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    //the only reducer we want to persist
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
 
+export default persistReducer(persistConfig, rootReducer);
