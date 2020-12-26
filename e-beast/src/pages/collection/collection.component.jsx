160265161d2288490b948e1ed09e8f491ac2e67b
Natalia Romankevich <romankevich.law@gmail.com>
2020-12-26 23:24:38 +0100

    styles for collection component

--- e-beast/src/pages/collection/collection.component.jsx
+++ e-beast/src/pages/collection/collection.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './collection.styles.scss';
 import CollectionItem  from '../../components/collection-item/collection-item.component';
+import { selectCollection } from '../../redux/shop/shop.selectors'; 
+
 
 const CollectionPage = ({ match }) => (
     <div className="collection-page">
@@ -8,4 +11,9 @@ const CollectionPage = ({ match }) => (
     </div>
 );
 
-export default CollectionPage;
+//ownProps are the props of the wrapped component
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
--- e-beast/src/redux/shop/shop.selectors.js
+++ e-beast/src/redux/shop/shop.selectors.js
@@ -1,4 +1,13 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
+
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+}
 
 const selectShop = state => state.shop;
 
@@ -6,3 +15,13 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
+
+//memoizing the return of the F, in case we call it again with the same param we won't rerurn it again, cause the result will be the same.
+export const selectCollection = memoize((selectCollectionParam) => 
+    createSelector(
+        [selectCollections],
+        collections => 
+            collections.find(
+                collection => collection.id === COLLECTION_ID_MAP[selectCollectionParam]
+            )
+    ));
