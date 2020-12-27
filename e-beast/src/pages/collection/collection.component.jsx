318b8c4bf30511485add49d3a20b0fc2324a9493
Natalia Romankevich <romankevich.law@gmail.com>
2020-12-27 12:02:45 +0100

    move our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- e-beast/src/pages/collection/collection.component.jsx
+++ e-beast/src/pages/collection/collection.component.jsx
@@ -5,11 +5,19 @@ import CollectionItem  from '../../components/collection-item/collection-item.co
 import { selectCollection } from '../../redux/shop/shop.selectors'; 
 
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+
+    return(
     <div className="collection-page">
-        <h2>COLLECTION PAGE</h2>
-    </div>
-);
+        <h2 className='title'>{ title }</h2>
+        <div className="items">
+            {items.map(item =>
+                <CollectionItem key={item.id} item={item}/>)
+            }
+        </div>
+    </div>)
+};
 
 //ownProps are the props of the wrapped component
 const mapStateToProps = (state, ownProps) => ({
--- e-beast/src/redux/shop/shop.selectors.js
+++ e-beast/src/redux/shop/shop.selectors.js
@@ -1,14 +1,6 @@
 import { createSelector } from 'reselect';
 import memoize from 'lodash.memoize';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -16,12 +8,14 @@ export const selectCollections = createSelector(
     shop => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollection],
+    collections => Object.keys(collections).map(key=> collections[key])
+);
+
 //memoizing the return of the F, in case we call it again with the same param we won't rerurn it again, cause the result will be the same.
-export const selectCollection = memoize((selectCollectionParam) => 
+export const selectCollection = memoize((collectionUrlParam) => 
     createSelector(
         [selectCollections],
-        collections => 
-            collections.find(
-                collection => collection.id === COLLECTION_ID_MAP[selectCollectionParam]
-            )
+        collections => collections[collectionUrlParam]     
     ));
