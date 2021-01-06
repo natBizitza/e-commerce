6225427c4b2bf4a3a95a90debfd2db050911476b
Natalia Romankevich <romankevich.law@gmail.com>
2021-01-06 22:27:39 +0100

    updated text checkout component

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -33,7 +33,7 @@ const CheckoutPage = ({cartItems, total}) => (
         }
         <div className="total">TOTAL : €{total}</div>
         <div className="test-warning">
-            *Please, use the following test credit card for payments*
+            * Please, use the following test credit card for payments *
             <br/>
             4242 4242 4242 4242 - Exp.: 01/21 - CVV: 123
         </div>
