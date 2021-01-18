3d555fd87d3059dbd375a9e93542dc079aca31a3
Natalia Romankevich <romankevich.law@gmail.com>
2021-01-18 22:35:51 +0100

    added styles to cartItem component, removed a horizontal scrollbar from cartDropdown component

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import './cart-item.styles.scss';
+import { CartItemContainer,ItemDetailsContainer,CartItemImage } from './cart-item.styles';
 
-const CartItem = ({item: { imageUrl, price, name, quantity}}) => (
-    <div className="cart-item">
-        <img src={imageUrl} alt='item'/>
-        <div className="item-details">
-            <span className="name">{name}</span>
-            <span className="price">{quantity} x €{price}</span>
-
-        </div>
-    </div>
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+    <CartItemContainer>
+      <CartItemImage src={imageUrl} alt='item' />
+      <ItemDetailsContainer>
+        <span>{name}</span>
+        <span>
+          {quantity} x ${price}
+        </span>
+      </ItemDetailsContainer>
+    </CartItemContainer>
 );
 
 export default React.memo(CartItem);
