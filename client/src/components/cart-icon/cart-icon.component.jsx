7508bcc39b408a2b943c42729b7a59320f24ed21
Natalia Romankevich <romankevich.law@gmail.com>
2021-01-20 23:05:07 +0100

    added styled components to cart icon component, also applied styles to ShoppingIcon, custom component

--- client/src/components/cart-icon/cart-icon.component.jsx
+++ client/src/components/cart-icon/cart-icon.component.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
-import './cart-icon.styles.scss';
+import {CartIconContainer, ShoppingIconContainer,ItemCountSpan} from './cart-icon.styles.jsx';
 
 const CartIcon = ({toggleCartHidden, itemCount}) => (
-    <div className="cart-icon" onClick={toggleCartHidden}>
-        <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>{itemCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleCartHidden}>
+        <ShoppingIconContainer className='shopping-icon'/>
+        <ItemCountSpan className='item-count'>{itemCount}</ItemCountSpan>
+    </CartIconContainer>
 );
 
 const mapDispatchToProps = dispatch => ({
