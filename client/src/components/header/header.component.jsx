017ea298261b1f08b28cefcf7d514552956a53e6
Natalia Romankevich <romankevich.law@gmail.com>
2021-01-21 21:48:27 +0100

    deleted unused imports and styles files

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
-import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
